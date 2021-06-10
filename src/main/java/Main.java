3c0896bc88784cfd35cd8ef58d562307c3a58332
Kseniia <xenia-nikole@mail.ru>
2021-06-10 16:21:13 +0300

    get image successfully

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.config.RequestConfig;
@@ -11,51 +13,68 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-
-    public static final String HTTP_URL =
-            "https://api.nasa.gov/planetary/apod?api_key=pPsb2pIy4gPSdhBRh0xhNYi0JWUq9lnYhpKPa3dD";
+    public static final String SERVICE_URL = "https://api.nasa.gov/planetary/apod?api_key=pPsb2pIy4gPSdhBRh0xhNYi0JWUq9lnYhpKPa3dD";
     public static ObjectMapper mapper = new ObjectMapper();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         CloseableHttpClient httpClient = HttpClientBuilder.create()
-                .setUserAgent("My test service")
+                .setUserAgent("Test service")
                 .setDefaultRequestConfig(RequestConfig.custom()
-                        .setConnectTimeout(5000)
-                        .setSocketTimeout(30000)
-                        .setRedirectsEnabled(false)
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
                         .build())
                 .build();
 
-        HttpGet request = new HttpGet(HTTP_URL);
+        // создание объекта запроса:
+        HttpGet request = new HttpGet(SERVICE_URL);
         request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
 
-        CloseableHttpResponse response = httpClient.execute(request);
-        // вывод полученных заголовков
-        Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
-        // чтение тела ответа
-        NASAData nasaData = mapper.readValue(response.getEntity().getContent(), NASAData.class);
-        //url из json
-        String jpg_url = nasaData.getUrl();
+        try {
+            // отправка запроса:
+            CloseableHttpResponse response = httpClient.execute(request);
+            // вывод полученных заголовков:
+            Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
 
-        // запрос по ссылке изображения
-        HttpGet requestUrl = new HttpGet(jpg_url);
-        CloseableHttpResponse responseUrl = httpClient.execute(requestUrl);
-        // вывод заголовков
-        Arrays.stream(responseUrl.getAllHeaders()).forEach(System.out::println);
+            // преобразование json
+            mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+            List<NASAData> jsonNasaData = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {
+            });
+            // вывод в консоль jsonNasaData:
+            System.out.println("\n\nAll data:\n");
+            jsonNasaData.forEach(System.out::println);
 
-        // чтение тела ответа
-        byte[] bytes = response.getEntity().getContent().readAllBytes();
+            // url из прочитанного json:
+            String JPG_URL = jsonNasaData.get(0).getUrl();
 
-        // создание файла
-        String fileName = jpg_url.substring(jpg_url.lastIndexOf("/") + 1);
-        createFile(fileName);
+            // новый объекта запроса:
+            HttpGet requestJpg = new HttpGet(JPG_URL);
+            requestJpg.setHeader(HttpHeaders.ACCEPT, ContentType.IMAGE_JPEG.getMimeType());
+            // отправка запроса:
+            response = httpClient.execute(requestJpg);
 
-        // запись в файл
-        try (FileOutputStream fos = new FileOutputStream(fileName, false)) {
-            fos.write(bytes, 0, bytes.length);
-        } catch (IOException e) {
+            // вывод полученных заголовков
+            Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+
+            // тело ответа в виде массива байт
+            byte[] bytes = response.getEntity().getContent().readAllBytes();
+
+            // имя файла
+            String fileName = "C:/Users/User/Desktop/" + JPG_URL.substring(JPG_URL.lastIndexOf("/") + 1);
+
+            // создание файла
+            createFile(fileName);
+
+            // запись в файл
+            try (FileOutputStream fos = new FileOutputStream(fileName, false)) {
+                fos.write(bytes,0,bytes.length);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 
@@ -65,7 +84,7 @@ public class Main {
         File newFile = new File(nameFile);
         try {
             if (newFile.createNewFile()) {
-                System.out.println("\n\nНовый файл'" + nameFile + "'.");
+                System.out.println("\n\nNew file'" + nameFile + " was created'.");
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
